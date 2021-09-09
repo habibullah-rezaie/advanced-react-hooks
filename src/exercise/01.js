2dd5f101d080598dd32653565c0d8ed9be7c55e5
Habibullah Rezaie <habibullah.rezaie.8@gmail.com>
2021-09-09 06:52:09 +0430

    Simulate traditional dispatch object with a type and switch statement

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -4,12 +4,12 @@
 import * as React from 'react'
 
 function Counter({initialCount = 0, step = 1}) {
-  const [state, setState] = React.useReducer(countReducer, {
+  const [state, dispatch] = React.useReducer(countReducer, {
     count: initialCount,
   })
 
   const {count} = state
-  const increment = () => setState(currentState => ({count: currentState.count + step}))
+  const increment = () => dispatch({type: 'INCREMENT', step})
   return <button onClick={increment}>{count}</button>
 }
 
@@ -17,10 +17,12 @@ function App() {
   return <Counter />
 }
 
-function countReducer(state, action) {
-  return {
-    ...state,
-    ...(typeof action === 'function' ? action(state) : action),
+function countReducer(state, {type, step}) {
+  switch (type) {
+    case 'INCREMENT':
+      return {...state, count: state.count + step}
+    default:
+      return state
   }
 }
 
